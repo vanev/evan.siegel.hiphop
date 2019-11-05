819d29a697a9e3d1b5ba4bd8e74cbbb6100a4475
Evan Siegel <siegel.evan@gmail.com>
2019-11-04 21:33:35 -0500

    Adds Grailed to Resume

--- data/resume.js
+++ data/resume.js
@@ -1,4 +1,15 @@
 export const items = {
+  0: {
+    id: 0,
+    category_id: 1,
+    title: "Staff Engineer",
+    company: "Grailed",
+    location: "New York, NY",
+    start_time: "2017-10",
+    end_time: null,
+    body: [],
+  },
+
   1: {
     id: 1,
     category_id: 1,
@@ -6,7 +17,7 @@ export const items = {
     company: "Harry's",
     location: "New York, NY",
     start_time: "2013-02",
-    end_time: null,
+    end_time: "2017-07",
     body: [
       "Built Harrys.com custom Ruby-on-Rails e-commerce platform, primarily focusing on the frontend.",
       "Member of Harrys.com start up team faciliating initial launch of company and website.",
@@ -18,8 +29,8 @@ export const items = {
       "Developed technical front-end phone screens and interview questions.",
       "On-boarded 3 frontend engineers, assisted with on-boarding many others.",
       "Developed and taught a 10-week long Javascript 101 class for Harry's employees (a few times).",
-      "Unpacked a lot of FreshDirect boxes; changed a lot of empty water coolers; ran a handful of board game nights."
-    ]
+      "Unpacked a lot of FreshDirect boxes; changed a lot of empty water coolers; ran a handful of board game nights.",
+    ],
   },
 
   2: {
@@ -31,8 +42,8 @@ export const items = {
     start_time: "2011-02",
     end_time: "2013-02",
     body: [
-      "Responsible for building involvio.com, an event promotion website for college students, and nustalgic.com, an event photo gallery website. Managed developer and designer; wrote HTML, CSS, Javascript, and Python in building Nustalgic and the front-end of Involvio."
-    ]
+      "Responsible for building involvio.com, an event promotion website for college students, and nustalgic.com, an event photo gallery website. Managed developer and designer; wrote HTML, CSS, Javascript, and Python in building Nustalgic and the front-end of Involvio.",
+    ],
   },
 
   3: {
@@ -44,8 +55,8 @@ export const items = {
     start_time: "2011-06",
     end_time: "2011-08",
     body: [
-      "10-week incubator program bringing together select student entrepreneurs in a collaborative environment to develop digital, media, and marketing ideas. Involvio selected winner, awarded $10,000 prize to help fund business."
-    ]
+      "10-week incubator program bringing together select student entrepreneurs in a collaborative environment to develop digital, media, and marketing ideas. Involvio selected winner, awarded $10,000 prize to help fund business.",
+    ],
   },
 
   4: {
@@ -56,7 +67,7 @@ export const items = {
     company: "Goucher College",
     location: "Baltimore, MD",
     start_time: "2008-09",
-    end_time: "2011-05"
+    end_time: "2011-05",
   },
 
   5: {
@@ -68,8 +79,8 @@ export const items = {
     start_time: "2010-06",
     end_time: "2010-08",
     body: [
-      "Shadowed the director of the Energia Partnership, assisting with day-to-day tasks."
-    ]
+      "Shadowed the director of the Energia Partnership, assisting with day-to-day tasks.",
+    ],
   },
 
   6: {
@@ -83,8 +94,8 @@ export const items = {
     end_time: "2009-08",
     body: [
       "former, Resident Advisor, June - August 2008",
-      "former, Work Staff/Dishwasher, June - August 2007"
-    ]
+      "former, Work Staff/Dishwasher, June - August 2007",
+    ],
   },
 
   7: {
@@ -97,8 +108,8 @@ export const items = {
     end_time: "2010-07",
     body: [
       "Chaired the Student Senate and the Executive Board, bodies of 50+ and 10 students, respectively. Responsible for an annual budget of $250,000. Enacted the New Deal for Goucher College, a $20,000 on-campus job’s program for students.",
-      "former, Vice President for Clubs, Feb. 2009-Aug. 2009"
-    ]
+      "former, Vice President for Clubs, Feb. 2009-Aug. 2009",
+    ],
   },
 
   8: {
@@ -110,8 +121,8 @@ export const items = {
     start_time: "2010-02",
     end_time: "2011-05",
     body: [
-      "Organized and supervised week-long games of Humans vs. Zombies, with a team of 5-7 moderators, for over 200 students. Created and implemented a series of missions encouraging teamwork, leadership, and excitement."
-    ]
+      "Organized and supervised week-long games of Humans vs. Zombies, with a team of 5-7 moderators, for over 200 students. Created and implemented a series of missions encouraging teamwork, leadership, and excitement.",
+    ],
   },
 
   9: {
@@ -123,8 +134,8 @@ export const items = {
     start_time: "2006-08",
     end_time: "2007-06",
     body: [
-      "Developed and instituted educational programming and ran social functions for one of the most active teen youth groups in the New York region."
-    ]
+      "Developed and instituted educational programming and ran social functions for one of the most active teen youth groups in the New York region.",
+    ],
   },
 
   10: {
@@ -134,7 +145,7 @@ export const items = {
     school: "Goucher College",
     location: "Baltimore, MD",
     start_time: "2007-08",
-    end_time: "2011-05"
+    end_time: "2011-05",
   },
 
   11: {
@@ -144,7 +155,7 @@ export const items = {
     school: "Patapsco High School",
     location: "Baltimore, MD",
     start_time: "2010-08",
-    end_time: "2010-12"
+    end_time: "2010-12",
   },
 
   12: {
@@ -154,23 +165,23 @@ export const items = {
     school: "Ikamva Lesizwe Public School",
     location: "Kenton-On-Sea, South Africa",
     start_time: "2010-05",
-    end_time: "2010-06"
-  }
+    end_time: "2010-06",
+  },
 };
 
 export const categories = {
   1: {
     id: 1,
-    name: "work"
+    name: "work",
   },
 
   2: {
     id: 2,
-    name: "leadership"
+    name: "leadership",
   },
 
   3: {
     id: 3,
-    name: "education"
-  }
+    name: "education",
+  },
 };
